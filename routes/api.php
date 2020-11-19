d9d33a49fdfc34ce648dbc428ec9a9c8d124977d
Phillip Rios <philliparmanrios@gmail.com>
2020-11-19 16:03:34 +0000

    Routes API > add routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PokemonController;
+use App\Http\Controllers\TrainerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +18,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('v1')->group(function () {
+    Route::apiResource('trainers', TrainerController::class);
+    Route::apiResource('pokemons', PokemonController::class);
+    Route::apiResource('classes', TrainerController::class);
+    Route::apiResource('types', TrainerController::class);
+    Route::apiResource('groups', TrainerController::class);
 });
